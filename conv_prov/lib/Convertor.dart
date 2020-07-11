400dbc0fd3dacde787b298314edbd76542504ac9
SomayaBegDev <67427609+SomayaBegDev@users.noreply.github.com>
2020-07-11 19:45:20 +0300

    Unworked TextField
    Unworked TextField

---
+++ conv_prov/lib/Convertor.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'ConvertorProvideor.dart';
+
+class Convertor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ConvertorProvidor convProv = Provider.of<ConvertorProvidor>(context);
+    return Container(
+      constraints: BoxConstraints(minWidth: 200, maxHeight: 300),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [Colors.pinkAccent, Colors.pink]),
+        borderRadius: BorderRadius.all(
+          Radius.circular(30),
+        ),
+      ),
+      padding: EdgeInsets.all(5.5),
+      margin: EdgeInsets.fromLTRB(15, 50, 15, 50),
+      child: Center(
+        child: CupertinoPicker(
+            itemExtent: 30,
+            onSelectedItemChanged: (index) {
+              convProv
+                  .setFromUnit(convProv.getSelectedMap().keys.toList()[index]);
+            },
+            backgroundColor: Colors.transparent,
+            children:
+                convProv.getSelectedMap().keys.map((f) => Text(f)).toList()),
+      ),
+    );
+  }
+}
+
+class Convertor2 extends StatelessWidget {
+  Map selMap;
+  @override
+  Widget build(BuildContext context) {
+    ConvertorProvidor convProv = Provider.of<ConvertorProvidor>(context);
+    return Container(
+      constraints: BoxConstraints(minWidth: 200, maxHeight: 300),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [Colors.pinkAccent, Colors.pink]),
+        borderRadius: BorderRadius.all(
+          Radius.circular(30),
+        ),
+      ),
+      padding: EdgeInsets.all(5.5),
+      margin: EdgeInsets.fromLTRB(15, 50, 15, 50),
+      child: Center(
+        child: CupertinoPicker(
+            itemExtent: 30,
+            onSelectedItemChanged: (index) {
+              convProv
+                  .setToUnit(convProv.getSelectedMap().keys.toList()[index]);
+            },
+            backgroundColor: Colors.transparent,
+            children:
+                convProv.getSelectedMap().keys.map((f) => Text(f)).toList()),
+      ),
+    );
+  }
+}
---
+++ conv_prov/lib/ConvertorProvideor.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'Constant.dart';
+
+class ConvertorProvidor extends ChangeNotifier {
+  String fromUnit = 'NIS';
+  String toUnit = 'NIS';
+  double textFieldValue = 1.0;
+  String result = "0";
+  int selectedIndex = 0;
+  Map selMap = ConvertorProvidor().getSelectedMap();
+
+  getSelectedMap() {
+    return unitsNav[selectedIndex];
+  }
+
+  setSelectedIndex(int i) {
+    this.selectedIndex = i;
+    notifyListeners();
+  }
+
+  setFromUnit(String value) {
+    this.fromUnit = value;
+    calculate();
+  }
+
+  setToUnit(String value) {
+    this.toUnit = value;
+    calculate();
+  }
+
+  setValue(String myValue) {
+    this.textFieldValue = double.parse(myValue);
+    calculate();
+  }
+
+  calculate() {
+    this.result = "${selMap[fromUnit] / selMap[toUnit] * textFieldValue}";
+    notifyListeners();
+  }
+
+
+
+}
